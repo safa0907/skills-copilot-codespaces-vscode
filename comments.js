0c31b44c2fe990dc4c8f563a3dfe314225fab8f0
Safa Ridene <74101237+safa0907@users.noreply.github.com>
2024-12-01 18:18:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,22 @@
-// create web server
-const express = require("express");
-const app = express();
+// Create web server
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+const port = 3000;
 
-// create a route
-app.get("/", (req, res) => {
-  res.send("Hello World");
+// Create server
+http.createServer(function(req, res) {
+    const q = url.parse(req.url, true);
+    const filename = "." + q.pathname;
+    fs.readFile(filename, function(err, data) {
+        if (err) {
+            res.writeHead(404, {'Content-Type': 'text/html'});
+            return res.end("404 Not Found");
+        }
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write(data);
+        return res.end();
+    });
+}).listen(port, function() {
+    console.log(`Server running at http://localhost:${port}/`);
 });
-
-// listen to port 3000
-app.listen(3000, () => {
-  console.log("listening on port 3000");
-});
-
-// install nodemon to auto restart server
-// npm install -g nodemon
-// run server with nodemon
-// nodemon comments.js
-// nodemon will restart server every time we make changes
